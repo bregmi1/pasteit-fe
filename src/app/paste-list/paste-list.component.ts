e488ca000f71a228620b3722c0d9903809390614
Bijay Regmi <regmibijay2@gmail.com>
2018-04-26 23:12:08 -0500

    Adds a basic PasteList component

---
+++ src/app/paste-list/paste-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'paste-list',
+    templateUrl: './paste-list.component.html',
+    styleUrls:['./paste-list.component.css']
+})
+export class PasteListComponent{}
