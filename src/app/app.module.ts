4b356a3804e46b9cc014f7684a185d041276f284
Bijay Regmi <regmibijay2@gmail.com>
2018-04-27 00:01:00 -0500

    Adds the router link to paste list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -14,7 +15,20 @@ import { HeaderLinksComponent } from './header-links/header-links.component';
         PasteListComponent,
         HeaderLinksComponent
     ],
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+            {
+                path: '', 
+                redirectTo: 'pastes',
+                pathMatch: 'full'
+            },
+            {
+                path: 'pastes',
+                component: PasteListComponent
+            }
+        ])
+    ],
     providers: [],
     bootstrap: [ AppComponent ]
  })
