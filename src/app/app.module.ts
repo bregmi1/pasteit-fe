f189548b6e569f609074d2ae80b9d0005bcabd0a
Bijay Regmi <regmibijay2@gmail.com>
2018-04-26 23:18:10 -0500

    Adds the basic implementation of header-link component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PasteListComponent } from './paste-list/paste-list.component';
+import { HeaderLinksComponent } from './header-links/header-links.component';
 
 
 @NgModule({
     declarations: [ 
         AppComponent,
-        HeaderComponent
+        HeaderComponent,
+        PasteListComponent,
+        HeaderLinksComponent
     ],
     imports: [BrowserModule],
     providers: [],
